4c7baf9da2322836f0380557c6d13892ddc94e24
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2020-01-03 18:50:38 +0700

    change text size for title

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -9,7 +9,7 @@ class Jumbotron extends Component {
                     <div className="col-md-12 col-xs-12 text-center">
                         <img src={ this.props.logo } alt="logo" height="150px"/>
                         <br />
-                        <h1 className="text-white">JUDO KOTA BATAM</h1>
+                        <h1 className="text-white text-title">JUDO KOTA BATAM</h1>
                         <span className="text-muted">Villa Hanglekir, Mega Legenda, Batam</span>
                     </div>
                 </div>
